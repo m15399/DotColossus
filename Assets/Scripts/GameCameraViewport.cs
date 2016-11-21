4279a8c924019e0e05ee3d2149804c72f747b0bb
Mark Gardner <m15399@gmail.com>
2016-11-21 14:36:49 -0800

    Refactor screen sizing

---
+++ Assets/Scripts/GameCameraViewport.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Changes the size of the game camera to match TheGameManager's screen size
+/// </summary>
+[ExecuteInEditMode]
+public class GameCameraViewport : MonoBehaviour {
+
+	public Camera cam;
+
+	void Update () {
+
+		float screenAspect = ((float)Screen.width) / Screen.height;
+		float targetAspect = TheGameManager.screenWidth / TheGameManager.screenHeight;
+
+		float aspectMultiplier = targetAspect / screenAspect;
+
+		Rect rect = new Rect();
+
+		if(aspectMultiplier < 1){
+			rect.width = aspectMultiplier;
+			rect.height = 1;
+			rect.center = new Vector2(.5f, .5f);
+
+		} else {
+			// Screen is very portrait-y
+			//
+			rect.height = 1.0f / aspectMultiplier;
+			rect.width = 1;
+			rect.center = new Vector2(.5f, .5f);
+			rect.y = 1 - rect.height;
+		}
+
+		cam.rect = rect;
+	}
+}
